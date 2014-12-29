bd294eaab645be194f98ba87170a0e75900877f3
Aaron Hickman <ahickman@ebay.com>
2014-12-29 14:03:42 -0700

    updated Gulp tasks to process Bower dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -9,16 +9,29 @@ var gulp = require('gulp')
   , notify = require('gulp-notify')
   , gulpif = require('gulp-if')
   , args = require('yargs').argv
-  , ngAnnotate = require('gulp-ng-annotate');
+  , ngAnnotate = require('gulp-ng-annotate')
+  , moment = require('moment')
+  , bowerFiles = require('main-bower-files')
+  , filter = require('gulp-filter');
 
+
+
+process.env.NODE_ENV = (args.prod) ? 'prod':'dev';
+var isProd = args.prod || false;
+var injectString = (isProd) ? './public/dist/*.min.js' : './public/dist/**/*.js';
+console.log(process.env.NODE_ENV );
+
+var timestamp = moment().format('YYYY_MM_D__hh_mm_ss_a');
+var bowerFilters = {
+  js: filter('*.js'),
+  css: filter('*.css')
+};
 
 //TODO - Concat and minify any CSS
 //TODO - Add LESS to project and include a Gulp task
 //TODO - Watch task needs to do more, its not working properly with the concat/minify tasks
-
-
-var isProd = args.prod;
-var injectString = (isProd) ? './public/dist/scripts.min.js' : './public/dist/**/*.js';
+//TODO - JSHint or JSLint
+//TODO - Swap clean for Del tasks
 
 //usage from terminal = 'gulp --prod'
 
@@ -26,10 +39,21 @@ gulp.task('default', ['inject'], function() {
   gulp.start('serve');
 });
 
-gulp.task('inject', ['build'],  function() {
+gulp.task('inject', ['buildBower'],  function() {
   return gulp.src('public/index.html')
-    .pipe(inject(gulp.src(injectString, {read: false}), {relative: true}))
+    .pipe(inject(gulp.src('./public/dist/*.js', {read: false}), {relative: true}))
+    .pipe(inject(gulp.src('./public/dist/bower/*.js', {read: false}), {name: 'bower', ignorePath: 'public'}))
     .pipe(gulp.dest('./public'));
+});
+
+gulp.task('buildBower', ['build'], function() {
+  return gulp.src(bowerFiles())
+    .pipe(bowerFilters.js)
+    .pipe(gulpif(isProd, concat('bowerScripts.js')))
+    .pipe(gulpif(isProd, rename({suffix: '.' + timestamp + '.min'})))
+    .pipe(gulpif(isProd, uglify()))
+    .pipe(notify({message: 'Processing: <%= file.relative %>'}))
+    .pipe(gulp.dest("./public/dist/bower"))
 });
 
 gulp.task('build', ['clean'], function() {
@@ -39,12 +63,14 @@ gulp.task('build', ['clean'], function() {
       add: true,
       single_quotes: true
     }))
-    .pipe(concat('scripts.js'))
-    .pipe(gulp.dest('./public/dist'))
-    .pipe(gulpif(isProd, rename({suffix: '.min'})))
+    .pipe(gulpif(isProd, concat('scripts.js')))
+    .pipe(gulpif(isProd, rename({suffix: timestamp  + '.min'})))
     .pipe(gulpif(isProd, uglify()))
-    .pipe(gulpif(isProd, gulp.dest('./public/dist')))
-    .pipe(notify({message: 'Scripts task complete'}));
+    .pipe(gulp.dest('./public/dist'))
+    .pipe(notify({message: 'Processing: <%= file.relative %>'}))
+    .pipe(notify({
+      message: "Build Task Complete"
+      , onLast: true }))
 });
 
 gulp.task('clean', function () {
